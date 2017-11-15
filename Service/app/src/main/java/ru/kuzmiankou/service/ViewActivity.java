8173f3b52f66801115dfd2ae136ea2b3ed4c0b21
kuzanatoliy <njkznu@gmail.com>
2017-11-15 22:03:18 +0300

    Create project

---
+++ Service/app/src/main/java/ru/kuzmiankou/service/ViewActivity.java
@@ -0,0 +1,61 @@
+package ru.kuzmiankou.service;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.io.File;
+import java.lang.reflect.Array;
+
+public class ViewActivity extends AppCompatActivity {
+    File filesDir = null;
+    File[] files = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final WebView webview = (WebView) findViewById(R.id.webview);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        filesDir = getFilesDir();
+        files = filesDir.listFiles();
+        String[] filesList = filesDir.list();
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String> (this, android.R.layout.simple_spinner_item, filesList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                webview.loadUrl("file:///android_res/raw/vk.html");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
